6e6a5a4da5d530ff0d9ed4609fce06bf7a03e5a5
Krisztian Buza <krisztian.buza80@gmail.com>
2020-05-05 21:30:32 +0100

    The profile model created and registered to the built-in admin site.

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    """
+    Model definition for the user profile
+    """
+
+    USER_TYPE = (
+        ('S', 'Student'),
+        ('T', 'Teacher'),
+    )
+
+    CLASSES = (
+        ('J_I', 'Junior infant'),
+        ('S_I', 'Senior infant'),
+        ('1ST', '1st class'),
+        ('2ND', '2nd class'),
+        ('3RD', '3rd class'),
+        ('4TH', '4th class'),
+        ('5TH', '5th class'),
+        ('6TH', '6th class'),
+    )
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField('User type', max_length=1, choices=USER_TYPE)
+    classname = models.CharField('Class', max_length=3, choices=CLASSES)
+    
+    
+    class Meta:
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+
+    def __str__(self):
+        """
+        Unicode representation of the user profile.
+        """
+        
+        return self.user.first_name + ' ' + self.user.last_name
