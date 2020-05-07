b4b186798cf7d40906c0ff5219d37a6c8ff68680
Krisztian Buza <krisztian.buza80@gmail.com>
2020-05-07 17:59:12 +0100

    Supported file validation added.

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Profile, ContentUpload
+import os
+from django.core.exceptions import ValidationError
 
 
 class ProfileForm(forms.ModelForm):
@@ -51,6 +53,26 @@ class ContentUploadForm(forms.ModelForm):
     A form to upload content
     """
 
+    content = forms.FileField(
+        label='Upload content',
+        required=True,
+        help_text='Upload .pdf, .jpg, .png, .doc, .docx, .xls or .xlsx file only.',
+    )
+
     class Meta:
         model = ContentUpload
         fields = ['title', 'content']
+
+    def clean_content(self, *args, **kwargs):
+        """
+        This function check if the file to upload is supported or not
+        """
+    
+        valid_extensions = ['.pdf', '.doc', '.docx', '.jpg', '.png', '.xlsx', '.xls']
+        content = self.cleaned_data.get('content')
+        ext = os.path.splitext(content.name)[1]
+
+        if not ext.lower() in valid_extensions:
+            raise ValidationError('Unsupported file.')
+
+        return content
