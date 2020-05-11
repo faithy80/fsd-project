d196a4a5305ce361a5efa03fd25d88557615a1d1
Krisztian Buza <krisztian.buza80@gmail.com>
2020-05-11 19:31:30 +0100

    A link to the student account organization added.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import dashboard, upload_content, delete_content
+from .views import dashboard, upload_content, delete_content, organize_a_student
 
 
 urlpatterns = [
     path('', dashboard, name='dashboard'),
     path('upload/', upload_content, name='upload_content'),
     path('delete/<int:pk>/', delete_content, name='delete_content'),
+    path('organize', organize_a_student, name='organize_a_student'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -15,18 +15,14 @@ def dashboard(request):
         return render(request, 'admin.html')
 
     else:
-        # get the profile of the user
+        # get the user's profile
         profile = get_object_or_404(Profile, user_id=request.user.id)
 
         # if the user is a teacher
         if profile.user_type == 'T':
-            student_choices = Profile.objects.filter(user_type='S').filter(classname=profile.classname)
-            select_form = ChooseStudentForm(student_choices=student_choices)
-
+           
+            context = {}
             # renders the teacher dashboard
-            context = {
-                'select_form': select_form,
-            }
             return render(request, 'teacher.html', context)
 
         # if the user is a student
@@ -34,12 +30,24 @@ def dashboard(request):
             uploaded_content = ContentUpload.objects.filter(user=request.user)
 
             context = {
-                'profile': profile,
                 'uploaded_content': uploaded_content,
             }
             # renders the student dashboard 
             return render(request, 'student.html', context)
 
+def organize_a_student(request):
+    # get the profile of the user
+    profile = get_object_or_404(Profile, user_id=request.user.id)
+
+    # gather information for the student selector
+    student_choices = Profile.objects.filter(user_type='S').filter(classname=profile.classname)
+    select_form = ChooseStudentForm(student_choices=student_choices)
+
+    context = {
+        'select_form': select_form,
+    }
+    # renders the view to organize the student
+    return render(request, 'organize_student.html', context)
 
 def upload_content(request):
     """
