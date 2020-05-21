7778154cea8369d8596e34f2c8470d80b500a7e3
Krisztian Buza <krisztian.buza80@gmail.com>
2020-05-21 16:27:31 +0100

    Django's default password reset views added.

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,30 @@
 from django.urls import path
 from .views import logout, login, register
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('logout/', logout, name='logout'),
     path('login/', login, name='login'),
     path('register/', register, name='register'),
+    path(
+        'password-reset/',
+        auth_views.PasswordResetView.as_view(),
+        name='password_reset',
+    ),
+    path(
+        'password-reset/done/',
+        auth_views.PasswordResetDoneView.as_view(),
+        name='password_reset_done',
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm',
+    ),
+    path(
+        'reset/done/',
+        auth_views.PasswordResetCompleteView.as_view(),
+        name='password_reset_complete',
+    ),
 ]
