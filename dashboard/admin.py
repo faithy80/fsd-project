21c47fae15f0f4a73d8e1c3bde16071cf99e12e1
Krisztian Buza <krisztian.buza80@gmail.com>
2020-06-18 11:03:01 +0100

    messages added to the django built-in admin panel

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
-from .models import Profile,ContentUpload
+from .models import Profile, ContentUpload, Messages
 
 # class to enable non-editable fields to display
 # for content upload model
 class ContentUploadAdmin(admin.ModelAdmin):
     readonly_fields = ('upload_date',)
 
+class MessagesAdmin(admin.ModelAdmin):
+    readonly_fields = ('message_date',)
+
 admin.site.register(Profile)
 admin.site.register(ContentUpload, ContentUploadAdmin)
+admin.site.register(Messages, MessagesAdmin)
