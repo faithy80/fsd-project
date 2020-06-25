1366fa3d1adc4f345d36d20ed475f44dae772798
Krisztian Buza <krisztian.buza80@gmail.com>
2020-06-25 13:09:14 +0100

    env-example.py created for variable setup guidance.

---
+++ env-example.py
@@ -0,0 +1,14 @@
+import os
+
+os.environ.setdefault(
+    'SECRET_KEY', ''
+)
+os.environ.setdefault(
+    'EMAIL_HOST_USER', ''
+)
+os.environ.setdefault(
+    'EMAIL_HOST_PASSWORD', ''
+)
+os.environ.setdefault(
+    'DATABASE_URL', ''
+)
