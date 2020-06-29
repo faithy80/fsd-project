dbb30f7b680aa07964214851c2df3572edd8fa6c
Krisztian Buza <krisztian.buza80@gmail.com>
2020-06-29 12:52:12 +0100

    Product model created.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from gdstorage.storage import GoogleDriveStorage
 
-# Create your models here.
+# Define Google Drive Storage
+gd_storage = GoogleDriveStorage()
+
+
+class Product(models.Model):
+    """
+    Model definition for the product
+    """
+
+    product_name = models.CharField(
+        'Product name',
+        max_length=50,
+    )
+    product_description = models.CharField(
+        'Product description',
+        max_length=200,
+    )
+    product_price = models.DecimalField(
+        max_digits=6,
+        decimal_places=2,
+    )
+    product_image = models.ImageField(
+        'Product image',
+        storage=gd_storage,
+        upload_to='product_images'
+    )
+    
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        """
+        Unicode representation of the product
+        """
+        
+        return self.product_name
