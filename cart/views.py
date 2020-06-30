a8ef3c32af54ec0581fbfd8eaff8f0466e9f22e3
Krisztian Buza <krisztian.buza80@gmail.com>
2020-06-30 17:49:47 +0100

    Add to cart functionality added.

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 def view_cart(request):
     context = {}
     return render(request, 'view_cart.html', context)
+
+
+def add_to_cart(request, pk):
+    quantity = int(request.POST.get('quantity'))
+    if request.method == 'POST':
+        if quantity > 10:
+            messages.error(request, "The limit is 10 for each product to buy.")
+        else:
+            # add product and quantity to the session cart
+            cart = request.session.get('cart', {})
+
+            if pk in list(cart.keys()):
+                cart[pk] += quantity
+            else:
+                cart[pk] = quantity
+
+            request.session['cart'] = cart
+
+            # send feedback
+            messages.success(request, "The product has been added to the cart.")
+    
+    # redirect to the previous page
+    return redirect(request.META.get('HTTP_REFERER'))
--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import shop
+from cart.views import add_to_cart
 
 
 urlpatterns = [
     path('', shop, name='shop'),
+    path('add_to_cart/<int:pk>/', add_to_cart, name='add_to_cart'),
 ]
