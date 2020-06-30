52b2db2c8fee9af826c5c9b5f20ac19cdc761a9b
Krisztian Buza <krisztian.buza80@gmail.com>
2020-06-30 18:58:16 +0100

    The cart context processor fetches data from the session cart.

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,4 +1,5 @@
-
+from django.shortcuts import get_object_or_404
+from shop.models import Product
 
 
 def cart_contents(request):
@@ -6,6 +7,19 @@ def cart_contents(request):
     cart_items = []
     price_total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        price_total += quantity * product.product_price
+        product_count += quantity
+        cart_items.append(
+            {
+                'item_id': item_id,
+                'quantity': quantity,
+                'product' : product,
+            }
+        )
 
     context = {
         'cart_items': cart_items,
