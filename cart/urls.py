6b220d5bf407ed9fa9d987881fecfb746712a7c5
Krisztian Buza <krisztian.buza80@gmail.com>
2020-07-01 12:21:13 +0100

    Add to cart url moved from the shop urls to the cart urls.

--- cart/urls.py
+++ cart/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import view_cart, update_cart
+from .views import *
 
 
 urlpatterns = [
     path('', view_cart, name='view_cart'),
+    path('add_to_cart/<int:item_id>/', add_to_cart, name='add_to_cart'),
     path('update/<int:item_id>/', update_cart, name='update_cart'),
 ]
--- shop/urls.py
+++ shop/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from .views import shop
-from cart.views import add_to_cart
 
 
 urlpatterns = [
     path('', shop, name='shop'),
-    path('add_to_cart/<int:item_id>/', add_to_cart, name='add_to_cart'),
 ]
