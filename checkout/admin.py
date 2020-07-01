b67fd1093ee4c0b96dabee1eee1358d407dbed5d
Krisztian Buza <krisztian.buza80@gmail.com>
2020-07-01 16:27:40 +0100

    Order and order item models added to the built-in admin dashboard.

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+# class to enable non-editable fields to display
+# for content upload model
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'order_number',
+        'order_date',
+    )
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
