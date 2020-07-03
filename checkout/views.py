9f754d933c104fbe26c70621f4b33948a3b141d4
Krisztian Buza <krisztian.buza80@gmail.com>
2020-07-03 12:21:06 +0100

    Basic stripe authentication added.

--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,41 @@
 from django.shortcuts import render
 from .forms import OrderForm
 from django.conf import settings
+from cart.contexts import cart_contents
 import stripe
 
 
 def checkout(request):
+    # check if the cart is empty
+    cart = request.session.get('cart', {})
+
+    if request.method == 'POST':
+        pass
+    else:
+        if not cart:
+            messages.error(request, 'There is nothing in your cart.')
+
+    # get stripe keys
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
+    # get the final price to pay
+    current_cart = cart_contents(request)
+    total = current_cart['price_total']
+    stripe_total = round(total * 100)
+
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
 
     context = {
         'order_form': order_form,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     # renders the order form view
--- static/js/client.js
+++ static/js/client.js
@@ -1,6 +1,8 @@
+// get keys from the view
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 
+// create a card element to display in view
 var stripe = Stripe(stripePublicKey, {
     locale: 'en'
 });
@@ -8,3 +10,40 @@ var elements = stripe.elements();
 var card = elements.create('card');
 
 card.mount('#card-element');
+
+// Real-time error messages displayed
+card.on('change', ({
+    error
+}) => {
+    const displayError = document.getElementById('card-errors');
+    if (error) {
+        displayError.textContent = error.message;
+    } else {
+        displayError.textContent = '';
+    }
+});
+
+// Form submit handler
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function (ev) {
+    ev.preventDefault();
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+            billing_details: {
+                name: 'test'
+            }
+        }
+    }).then(function (result) {
+        const displayError = document.getElementById('card-errors');
+        if (result.error) {
+            displayError.textContent = error.message;
+        } else {
+            // The payment has been processed!
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
